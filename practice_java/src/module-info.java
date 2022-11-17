72492eb32f7e6b58dbb9c8bf91263a71b1a666bc
satyam <satyammahale96@outlook.com>
2022-11-17 12:28:33 +0530

    this is my first java project

---
+++ practice_java/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author satya
+ *
+ */
+module practice_java {
+}
---
+++ practice_java/src/practice_java/Loops_Java.java
@@ -0,0 +1,20 @@
+package practice_java;
+
+public class Loops_Java {
+	public static void main(String[] args) {
+		int n = 50;
+		System.out.println("even number is");
+		for (int i=0; i<n;i++) {
+		
+			System.out.print(2*i+"|");
+			
+		}
+		
+		/*int n=5;
+		for (int i=n;i>0;i--) {
+		System.out.println(i);	
+		}*/
+		
+	}
+
+}
